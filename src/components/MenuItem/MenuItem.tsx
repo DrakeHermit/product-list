5c5141cbb836e59c3c256402c2ec70696ed68215
Jan Kotvaš <jan.kotvas96@gmail.com>
2025-03-21 13:24:26 +0100

    feat: added responsiveness to the list items for tablet and desktop devices

--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -5,17 +5,19 @@ export const MenuItem = () => {
   const products: ProcessedData[] = transformJSON()
   
   return (
-    <article>
-      {products.map((product: ProcessedData) => {
-        return (
-          <div key={product.id} className="menu__item">
-            <img src={product.image.mobile} alt={product.name} />
+    <ul>
+      {products.map((product: ProcessedData) => ( 
+          <li key={product.id} className="menu__item">
+            <picture>
+              <source media='(min-width: 1050px)' srcSet={product.image.desktop} />
+              <source media='(min-width: 375px)' srcSet={product.image.tablet} />
+              <img src={product.image.mobile} alt={product.name} />
+            </picture>
             <p className='menu__category'>{ product.category }</p>
             <h3 className='menu__item-header'>{product.name}</h3>
             <p className='menu__price'>${product.price.toFixed(2)}</p>
-          </div>
-        )
-      })}
-    </article>
+          </li>
+      ))}
+    </ul>
     )
 }
