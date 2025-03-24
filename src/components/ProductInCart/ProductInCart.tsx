56c7fc0342a33d077773e3d07bf91c37469dcea2
Jan Kotvaš <jan.kotvas96@gmail.com>
2025-03-24 19:38:28 +0100

    fix: fixed the shopping cart design inconsistencies

--- src/components/ProductInCart/ProductInCart.tsx
+++ src/components/ProductInCart/ProductInCart.tsx
@@ -2,7 +2,8 @@ import "./ProductInCart.css"
 
 export const ProductInCart = () => {
   return (
-    <ul>
+    <>
+      <ul>
         <li className="cart__product">
           <div className="cart__product-info">
             <h3>Classic Tiramisu</h3>
@@ -17,6 +18,16 @@ export const ProductInCart = () => {
           </button>
         </li>
         <hr />
-    </ul>
+      </ul>
+      <div className="cart__total">
+        <p>Order Total</p>
+        <p className="cart__total-price">$46.50</p>
+      </div>
+      <div className="cart__vision">
+        <img src="/src/assets/images/icon-carbon-neutral.svg" alt="" />
+        <p>This is a <span className="bold">carbon-neutral</span> delivery</p>
+      </div>
+      <button className="cart__confirm">Confirm Order</button>
+    </>
   )
 }
