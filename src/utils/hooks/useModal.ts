6fda9c23eed0d7965f256e310359f00151a286c4
Jan Kotvaš <jan.kotvas96@gmail.com>
2025-04-04 08:48:46 +0200

    feat: added the functionality to close the modal with escape button also stopped the body from overflowing when modal is opened

--- src/utils/hooks/useModal.ts
+++ src/utils/hooks/useModal.ts
@@ -1,7 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const useModal = () => { 
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("keydown", handleEsc);
+
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset"; 
+    }
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+      document.body.style.overflow = "unset"; 
+    };
+  }, [isOpen]);
 
   const openModal = () => setIsOpen(true);
 
